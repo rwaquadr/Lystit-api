3d2e14f9e46ac911bf1296a69ca9e55c08609b50
Robb Aquadro <rwaquadr@gmail.com>
2017-07-27 14:36:49 -0500

    changed ports

--- server.js
+++ server.js
@@ -9,19 +9,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 5000, function(err) {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-  console.log(`Magic is happening on ${process.env.PORT}`)
-});
-
-// app.listen(port, function(err) {  
-//  if (err) {
-//    return console.log('something bad happened', err)
-//  }
-//  console.log(`Magic is happening on ${port}`)
+// app.listen(process.env.PORT || 5000, function(err) {  
+//   if (err) {
+//     return console.log('something bad happened', err)
+//   }
+//   console.log(`Magic is happening on ${process.env.PORT}`)
 // });
+
+app.listen(port, function(err) {  
+ if (err) {
+   return console.log('something bad happened', err)
+ }
+ console.log(`Magic is happening on ${port}`)
+});
 
 
 //Connect to Mongo
